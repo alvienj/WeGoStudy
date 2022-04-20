d02c13625e3f7a52674e9c09fb1613f4cae2a06b
alvienj <99428780+alvienj@users.noreply.github.com>
2022-04-20 14:11:22 -0700

    Add files via upload

---
+++ Testing Environment.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import locators as locators
+from time import sleep
+import datetime
+
+s = Service('C:/Users/User/PycharmProjects/si/WeGoStudy/chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+
+driver.maximize_window()
+driver.implicitly_wait(30)
+driver.get(locators.wegostudy_url)
+
+
+def Setup():
+    if driver.current_url == locators.wegostudy_url and driver.title == 'WeGoStudy':
+        print('Were at the WeGoStudy homepage')
+    else:
+        print('Wrong website. Please try again')
+        driver.close()
+        driver.quit()
+    sleep(.5)
